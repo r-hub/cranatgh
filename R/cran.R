b58a2aaa8d6885bb6d46d16b785081d3bc95a367
Gábor Csárdi <csardi.gabor@gmail.com>
2023-03-21 15:06:49 +0100

    Fix version number fields for lme
    Some lme versions contain the date after a space.

--- R/cran.R
+++ R/cran.R
@@ -1,6 +1,6 @@
 
 get_cran_versions <- function(package) {
-  tryCatch({
+  tb <- tryCatch({
     set_names(
       pkgsearch::cran_package_history(package)[, c("date", "Version")],
       c("date", "version")
@@ -8,6 +8,12 @@ get_cran_versions <- function(package) {
   }, package_not_found_error = function(e) {
     tibble::tibble(date = character(), version = character())
   })
+
+  # some old version strings have an extra date after a space, e.g.
+  # lme 3.0-0 (1999/06/26)
+  # remove everything after a space
+  tb$version <- sub(" .*$", "", tb$version)
+  tb
 }
 
 #' Get the names and version numbers of all CRAN packages
