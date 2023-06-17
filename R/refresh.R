8f654785a870493a0793307a6a25ed2444e3cd7e
Gábor Csárdi <csardi.gabor@gmail.com>
2023-06-17 16:36:58 +0200

    Simplify refresh_status_yml()
    Take the package names from CRAN, that's much faster.

--- R/refresh.R
+++ R/refresh.R
@@ -1,27 +1,28 @@
+
 #' Refresh cranatgh-status.yml
 #'
 #' Do a full refresh of `cranatgh-status.yml` by scraping all github
 #' repositories from the CRAN organization.
 #' @export
-refresh_status_yml <- function(){
-  repos <- gh::gh("/orgs/cran/repos", .limit = 1e5)
-  packages <- vapply(repos, function(x){x$name}, character(1))
+
+refresh_status_yml <- function() {
+  packages <- get_all_cran_packages()$package
   versions <- rep(NA, length(packages))
 
   # Scrape versions from Metacran
   pool <- curl::new_pool(multiplex = FALSE)
   completed <- 0
-  fetch_version <- function(i){
+  fetch_version <- function(i) {
     url <- sprintf('https://raw.githubusercontent.com/cran/%s/master/DESCRIPTION', packages[i])
-    curl::curl_fetch_multi(url, done = function(res){
-      if(res$status_code == 200){
+    curl::curl_fetch_multi(url, done = function(res) {
+      if (res$status_code == 200) {
         try({
           versions[i] <<- parse_description_version(res$content)
         })
         completed <<- completed + 1
-        if(completed %% 100 == 0)
+        if (completed %% 100 == 0)
           message(sprintf("Completed %d of %d", completed, length(packages)))
-      } else if(res$status_code %in% c(403, 429)){
+      } else if (res$status_code %in% c(403, 429)) {
         message("Possibly hitting GH limit, waiting for a few seconds before retrying...")
         Sys.sleep(30)
         fetch_version(i)
@@ -40,7 +41,7 @@ refresh_status_yml <- function(){
 }
 
 # Get 'Version' field from DESCRIPTION
-parse_description_version <- function(buf){
+parse_description_version <- function(buf) {
   con <- rawConnection(buf)
   on.exit(close(con))
   desc <- read.dcf(con)
