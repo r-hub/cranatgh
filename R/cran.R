629a4370be0caba6467ae13b4f390662452adf8e
Gábor Csárdi <csardi.gabor@gmail.com>
2023-07-10 14:50:12 +0200

    Use pkgcache for metadata

--- R/cran.R
+++ R/cran.R
@@ -1,13 +1,36 @@
 
+archive_cache <- local({
+  cac <- NULL
+  function() {
+    if (is.null(cac)) {
+      cac <<- pkgcache::cran_archive_cache$new(cran_mirror = cran_mirror)
+    }
+    cac
+  }
+})
+
+metadata_cache <- local({
+  cmt <- NULL
+  function() {
+    if (is.null(cmt)) {
+      cmt <<- pkgcache::cranlike_metadata_cache$new(
+        platforms = "source",
+        bioc = FALSE
+      )
+    }
+    cmt
+  }
+})
+
 get_cran_versions <- function(package) {
-  tb <- tryCatch({
-    set_names(
-      pkgsearch::cran_package_history(package)[, c("date", "Version")],
-      c("date", "version")
-    )
-  }, package_not_found_error = function(e) {
-    tibble::tibble(date = character(), version = character())
-  })
+  archive <- archive_cache()$list(packages = package)
+  current <- metadata_cache()$list(packages = package)
+
+  tb <- data.frame(
+    stringsAsFactors = FALSE,
+    date = c(archive$mtime, current$published),
+    version = c(archive$version, current$version)
+  )
 
   # some old version strings have an extra date after a space, e.g.
   # lme 3.0-0 (1999/06/26)
@@ -29,50 +52,23 @@ get_cran_versions <- function(package) {
 #' @keywords internal
 
 get_all_cran_packages <- function() {
-  packages <- get_packages_rds()
-  archive <- get_archive_rds()
-  archive <- archive[vapply(archive, NROW, integer(1)) > 0]
+  archive <- archive_cache()$list()
+  current <- metadata_cache()$list()
 
-  pkgs <- unique(c(packages[, "Package"], names(archive)))
-  cur_ver <- structure(packages[, "Version"], names = packages[, "Package"])
-  old_ver <- na.omit(vapply(
-    archive,
-    function(x) {
-      n <- basename(rownames(x))
-      v <- sub("\\.tar\\.gz$", "", sub("^.*_", "", n))
-      pv <- package_version(v, strict=FALSE)
-      latest <- stats::na.omit(v[which(pv == max(pv))])
-      c(latest, NA_character_)[1]
-    },
-    character(1)
-  ))
-  ver <- ifelse(pkgs %in% names(cur_ver), unname(cur_ver[pkgs]), unname(old_ver[pkgs]))
-  df <- data.frame(
-    stringsAsFactors = FALSE,
-    package = pkgs,
-    version = ver
+  pkgs <- data.frame(
+    stringsAsFactore = FALSE,
+    package = c(archive$package, current$package),
+    version = c(archive$version, current$version)
   )
 
-  df
-}
+  drev <- function(x) {
+    x[rev(seq_len(nrow(x))), ]
+  }
 
-get_packages_rds <- function() {
-  # TODO: cache
-  url <- "https://cloud.r-project.org/src/contrib/PACKAGES.rds"
-  tmp <- tempfile()
-  on.exit(unlink(tmp))
-  utils::download.file(url, tmp, quiet = TRUE)
-  readRDS(tmp)
-}
-
-get_archive_rds <- function() {
-  # TODO: cache
-  url <- "https://cloud.r-project.org/src/contrib/Meta/archive.rds"
-  read_remote_rds(url)
-}
-
-read_remote_rds <- function(rds) {
-  con <- url(rds)
-  on.exit(close(con))
-  readRDS(con)
+  # Keep the last version for every package
+  # GitHub is case sensitive, so we merge packages that differ by case only.
+  pkgs <- drev(pkgs)
+  pkgs <- pkgs[!duplicated(tolower(pkgs$package)), ]
+  pkgs <- drev(pkgs)
+  pkgs
 }
